365a35b03151de1154ce4a2837cfd3f9557ac3b6
Ruben Reusser <rr@headwire.com>
2021-05-03 18:56:52 -0700

    initial product list and product detail component

---
+++ fragments/productdetail/template.vue
@@ -0,0 +1,10 @@
+<template>
+  <div v-bind:data-per-path="model.path">productdetail</div>
+</template>
+
+<script>
+    export default {
+        props: ['model']
+    }
+</script>
+
---
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/productlist/template.vue
@@ -0,0 +1,10 @@
+<template>
+  <div v-bind:data-per-path="model.path">productlist</div>
+</template>
+
+<script>
+    export default {
+        props: ['model']
+    }
+</script>
+
