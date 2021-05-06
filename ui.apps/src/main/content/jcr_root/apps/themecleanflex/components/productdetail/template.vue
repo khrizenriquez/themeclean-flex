591fd194903d8c56a21a2244a0514fc13b8ec376
Chris Enríquez <christoferen7@gmail.com>
2021-05-06 16:15:15 -0600

    Added static html for product list and product detail components

--- ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/productdetail/template.vue
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/productdetail/template.vue
@@ -1,5 +1,232 @@
 <template>
   <div v-bind:data-per-path="model.path">productdetail</div>
+
+  <div class="title aem-GridColumn aem-GridColumn--default--12">
+    <div id="title-6abc8848c3" class="cmp-title">
+      <h1 class="cmp-title__text">Single Product</h1>
+    </div>
+
+
+
+    <!--cq{"decorated":true,"type":"wknd/components/content/title","path":"/content/wknd-jecis/en/shopping/single-product/jcr:content/root/responsivegrid/title","selectors":null,"servlet":"Script /apps/core/wcm/components/title/v2/title/title.html","totalTime":1,"selfTime":1}-->
+    <cq data-path="/content/wknd-jecis/en/shopping/single-product/jcr:content/root/responsivegrid/title" data-config="{&quot;path&quot;:&quot;/content/wknd-jecis/en/shopping/single-product/jcr:content/root/responsivegrid/title&quot;,&quot;slingPath&quot;:&quot;/content/wknd-jecis/en/shopping/single-product/jcr:content/root/responsivegrid/title.html&quot;,&quot;dialog&quot;:&quot;/apps/core/wcm/components/title/v2/title/cq:dialog&quot;,&quot;dialogLoadingMode&quot;:&quot;auto&quot;,&quot;dialogLayout&quot;:&quot;auto&quot;,&quot;dialogSrc&quot;:&quot;/mnt/override/apps/core/wcm/components/title/v2/title/_cq_dialog.html/content/wknd-jecis/en/shopping/single-product/jcr:content/root/responsivegrid/title&quot;,&quot;designDialog&quot;:&quot;/apps/core/wcm/components/title/v2/title/cq:design_dialog&quot;,&quot;designDialogLayout&quot;:&quot;fullscreen&quot;,&quot;designDialogLoadingMode&quot;:&quot;auto&quot;,&quot;designDialogSrc&quot;:&quot;/mnt/override/apps/core/wcm/components/title/v2/title/_cq_design_dialog.policydesign.editabletemplate.html/conf/wknd/settings/wcm/policies/wknd/components/content/title/policy_1554340232282?dialog=%2Fapps%2Fcore%2Fwcm%2Fcomponents%2Ftitle%2Fv2%2Ftitle%2Fcq%3Adesign_dialog&amp;referrer=%2Fcontent%2Fwknd-jecis%2Fen%2Fshopping%2Fsingle-product&amp;policyContentPath=%2Fcontent%2Fwknd-jecis%2Fen%2Fshopping%2Fsingle-product%2Fjcr%3Acontent%2Froot%2Fresponsivegrid%2Ftitle&amp;policyContentResourceType=wknd%2Fcomponents%2Fcontent%2Ftitle&amp;policyDirectoryPath=%2Fconf%2Fwknd%2Fsettings%2Fwcm%2Fpolicies%2Fwknd%2Fcomponents%2Fcontent%2Ftitle%2F&amp;policyMappingPath=%2Fconf%2Fwknd%2Fsettings%2Fwcm%2Ftemplates%2Farticle-page-template%2Fpolicies%2Fjcr%3Acontent%2Froot%2Fresponsivegrid%2Ftitle&amp;policyMappingResourceType=wcm%2Fcore%2Fcomponents%2Fpolicies%2Fmapping&amp;policyName=policy_1554340232282&amp;policyRelativePath=wknd%2Fcomponents%2Fcontent%2Ftitle%2Fpolicy_1554340232282&amp;resourceType=wcm%2Fcore%2Fcomponents%2Fpolicy%2Fpolicy&quot;,&quot;designDialogClassic&quot;:&quot;/apps/core/wcm/components/title/v2/title/cq:design_dialog&quot;,&quot;hasPolicyMapping&quot;:true,&quot;type&quot;:&quot;wknd/components/content/title&quot;,&quot;isResponsiveGrid&quot;:false,&quot;policyPath&quot;:&quot;root/responsivegrid/title&quot;,&quot;csp&quot;:&quot;page|basicpage/root|responsivegrid/responsivegrid/title&quot;,&quot;editConfig&quot;:{&quot;inplaceEditingConfig&quot;:{&quot;active&quot;:true,&quot;editorType&quot;:&quot;title&quot;,&quot;configPath&quot;:&quot;/apps/core/wcm/components/title/v2/title/cq:editConfig/cq:inplaceEditing/inplaceEditingConfig&quot;},&quot;structure&quot;:true},&quot;ipeConfig&quot;:{&quot;jcr:primaryType&quot;:&quot;nt:unstructured&quot;,&quot;titleTag&quot;:[&quot;h1&quot;,&quot;h2&quot;,&quot;h3&quot;,&quot;h4&quot;,&quot;h5&quot;,&quot;h6&quot;],&quot;inplaceEditingConfig&quot;:{&quot;jcr:primaryType&quot;:&quot;nt:unstructured&quot;,&quot;titleTag&quot;:[&quot;h1&quot;,&quot;h2&quot;,&quot;h3&quot;,&quot;h4&quot;,&quot;h5&quot;,&quot;h6&quot;]}}}"></cq>
+  </div>
+  <div class="responsivegrid aem-GridColumn--tablet--12 aem-GridColumn--offset--tablet--0 aem-GridColumn--default--none aem-GridColumn--phone--none aem-GridColumn--phone--12 aem-GridColumn--tablet--none aem-GridColumn--default--9 aem-GridColumn aem-GridColumn--offset--phone--0 aem-GridColumn--offset--default--0">
+
+
+    <div class="aem-Grid aem-Grid--9 aem-Grid--default--9 ">
+
+      <div class="we-jecisProductList aem-GridColumn--default--9 aem-GridColumn"><style>
+        /*
+        https://codepen.io/fahrettinaksoy/pen/VKwqwR
+        */
+        .wrapper {
+          margin: 50px auto;
+          width: 280px;
+          height: 370px;
+          background: white;
+          border-radius: 10px;
+          position: relative;
+          z-index: 90;
+        }
+
+        .ribbon-wrapper-red {
+          width: 100px;
+          height: 100px;
+          overflow: hidden;
+          position: absolute;
+          top: -3px;
+          right: -3px;
+          z-index: 99;
+        }
+        .ribbon-red {
+          font: bold 15px Sans-Serif;
+          color: #fff;
+          text-align: center;
+          text-shadow: rgba(255,255,255,0.5) 0px 1px 0px;
+          -webkit-transform: rotate(45deg);
+          -moz-transform: rotate(45deg);
+          -ms-transform: rotate(45deg);
+          -o-transform: rotate(45deg);
+          position: relative;
+          padding: 7px 0;
+          left: -5px;
+          top: 15px;
+          width: 120px;
+          background-color: #2980b9;
+          background-image: -webkit-gradient(linear, left top, left bottom, from(#3498db), to(#2980b9));
+          background-image: -webkit-linear-gradient(top, #3498db, #2980b9);
+          background-image: -moz-linear-gradient(top, #3498db, #8EBF45);
+          background-image: -ms-linear-gradient(top, #3498db, #8EBF45);
+          background-image: -o-linear-gradient(top, #3498db, #8EBF45);
+          color: #fff;
+          -webkit-box-shadow: 0px 0px 3px rgba(0,0,0,0.3);
+          -moz-box-shadow: 0px 0px 3px rgba(0,0,0,0.3);
+          box-shadow: 0px 0px 3px rgba(0,0,0,0.3);
+        }
+
+        .ribbon-red:before,
+        .ribbon-red:after{
+          content: "";
+          border-top: 3px solid #3498db;
+          border-left: 3px solid transparent;
+          border-right: 3px solid transparent;
+          position:absolute;
+          bottom: -3px;
+        }
+
+        .ribbon-red:before{
+          left: 0;
+        }
+
+        .ribbon-red:after{
+          right: 0;
+        }
+      </style>
+
+        <div class="page-loader">
+          <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
+        </div>
+
+
+
+        <link rel="stylesheet" href="/apps/wkndjecis/components/content/jecisSingleProduct/clientlibs.css" type="text/css">
+        <link rel="stylesheet" href="/apps/wkndjecis/components/content/jecisProductList/clientlibs.css" type="text/css">
+
+
+
+
+
+        <script type="text/javascript" src="/apps/wkndjecis/components/content/jecisSingleProduct/clientlibs.js"></script>
+        <script type="text/javascript" src="/apps/wkndjecis/components/content/jecisProductList/clientlibs.js"></script>
+
+
+
+
+        <!-- URL Params ?profile={}&store={} -->
+
+
+
+
+
+
+
+        <div class="alert alert-primary hide" role="alert">
+          <h5>Product <a href="#" class="alert-link">Cuzco Blue</a> added to the cart</h5>
+        </div>
+
+        <div class="single">
+          <div>
+            <button onclick="return window.location.href = document.referrer;" class="btn btn-dark cart back-button">Back</button>
+          </div>
+
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-5 grid">
+                <div class="flexslider">
+
+                  <div class="ribbon-wrapper-red">
+                    <div class="ribbon-red">PROMO</div>
+                  </div>
+                  <ul class="slides">
+                    <li data-thumb="/content/dam/we-retail/en/products/activities/hiking/backpack/Frame 13.jpg">
+                      <img src="/content/dam/we-retail/en/products/activities/hiking/backpack/Frame%2013.jpg" data-imagezoom="true" class="img-responsive"/>
+                    </li>
+
+                    <li data-thumb="/content/dam/we-retail/en/products/activities/hiking/backpack/Frame 8.jpg">
+                      <img src="/content/dam/we-retail/en/products/activities/hiking/backpack/Frame%208.jpg" data-imagezoom="true" class="img-responsive"/>
+                    </li>
+
+                    <li data-thumb="/content/dam/we-retail/en/products/activities/hiking/backpack/Frame 1.jpg">
+                      <img src="/content/dam/we-retail/en/products/activities/hiking/backpack/Frame%201.jpg" data-imagezoom="true" class="img-responsive"/>
+                    </li>
+                  </ul>
+
+                </div>
+              </div>
+
+              <div class="col-md-7 single-top-in">
+                <div class="single-para simpleCart_shelfItem">
+                  <h2>Cuzco Blue</h2>
+                  <p></p><p>The super-power Backpack</p><p></p>
+                  <div class="star-on" style="display:none;">
+                    <ul>
+                      <li><a href="#"><i class="glyphicon glyphicon-star"> </i></a></li>
+                      <li><a href="#"><i class="glyphicon glyphicon-star"> </i></a></li>
+                      <li><a href="#"><i class="glyphicon glyphicon-star"> </i></a></li>
+                      <li><a href="#"><i class="glyphicon glyphicon-star"> </i></a></li>
+                      <li><a href="#"><i class="glyphicon glyphicon-star"> </i></a></li>
+                    </ul>
+                    <div class="review">
+                      <a href="#"> 3 reviews </a>
+                      <a href="#">  Write a review</a>
+                    </div>
+                    <div class="clearfix"> </div>
+                  </div>
+                  <h4 class="add-to item_price promo-original-price">
+                    <span id="itemCurrencyField">€</span>
+                    <span id="itemPriceField">210.00</span>
+                  </h4>
+                  <h4 class="add-to item_price small promo-discount-price">
+                    <s>
+                      <span>€</span>
+                      <span id="itemOriginalField">210.00</span>
+                    </s>
+                  </h4>
+                  <div class="available">
+                    <h6>Available Options :</h6>
+                    <ul>
+                      <li>
+                        Size
+                        <select class="custom-select" id="productSize">
+
+                          <option value="height">Height</option>>
+
+                          <option value="width">Width</option>>
+
+                        </select>
+                      </li>
+                      <li>
+                        Cost
+                        <select class="custom-select" id="productCost">
+                          <option value="us-dollar">U.S. Dollar</option>
+                          <option value="euro">Euro</option>
+                        </select>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="add-to-cart-section form-row">
+                    <div class="form-group col-sm-6">
+                      <select class="form-control" name="productQty" id="productQty">
+                        <option value="">Qty</option>
+                        <option value="1">Qty: 1</option>
+                        <option value="2">Qty: 2</option>
+                        <option value="3">Qty: 3</option>
+                        <option value="4">Qty: 4</option>
+                        <option value="5">Qty: 5</option>
+                      </select>
+                    </div>
+
+                    <div class="form-group col-sm-6">
+                      <button class="btn cart item_add view-all-border item_add btn-large">Add To Cart</button>
+                      <!--    <a href="#" class="cart item_add">More details</a> -->
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="clearfix"> </div>
+              <div class="content-top1">
+                <div class="clearfix"> </div>
+              </div>
+            </div>
+          </div>
+          <div class="clearfix"> </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
--- ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/productlist/template.vue
+++ ui.apps/src/main/content/jcr_root/apps/themecleanflex/components/productlist/template.vue
@@ -1,5 +1,221 @@
 <template>
   <div v-bind:data-per-path="model.path">productlist</div>
+
+  <div class="row">
+    <div class="col-md-12">
+      <div class="row row-eq-height">
+
+        <!-- Testing how to retireve the profile id, convert to a Sightly variable and instantiate another model class -->
+        <div class="col-md-4 text-center product-list-item">
+          <a href="/content/wknd-jecis/en/shopping/single-product.html?profile=170227049&store=wknd">
+            <div class="simpleCart_shelfItem wrapper">
+              <div class="ribbon-wrapper-red">
+                <div class="ribbon-red">PROMO</div>
+              </div>
+
+
+
+              <img src="/content/dam/we-retail/en/products/activities/hiking/170227049.jpg" class="img-responsive"/>
+
+
+
+              <h3><a href="/content/wknd-jecis/en/shopping/single-product.html?profile=170227049&store=wknd"> </a></h3>
+              <div class="price">
+                <h5></h5>
+
+                <button data-prod-discount="0" data-prod-symbol="€" data-prod-price="79.99" data-prod-prof="170227049" class="btn cart item_add view-all-border item_add btn-large">Add To Cart</button>
+                <div class="clearfix"></div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <!-- Testing how to retireve the profile id, convert to a Sightly variable and instantiate another model class -->
+
+
+
+
+
+        <div class="col-md-4 text-center product-list-item">
+          <a href="/content/wknd-jecis/en/shopping/single-product.html?profile=eqsusurud&store=wknd">
+            <div class="simpleCart_shelfItem wrapper">
+              <div class="ribbon-wrapper-red">
+                <div class="ribbon-red">PROMO</div>
+              </div>
+
+
+
+              <img src="/content/dam/we-retail/en/products/activities/surfing/source/Rusty%20Parrot.jpg" class="img-responsive" alt="Rusty Parrot Shortboard"/>
+
+
+
+              <h3><a href="/content/wknd-jecis/en/shopping/single-product.html?profile=eqsusurud&store=wknd"> Rusty Parrot Shortboard</a></h3>
+              <div class="price">
+                <h5></h5>
+
+                <button data-prod-discount="0" data-prod-symbol="€" data-prod-price="895.00" data-prod-prof="eqsusurud" class="btn cart item_add view-all-border item_add btn-large">Add To Cart</button>
+                <div class="clearfix"></div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <!-- Testing how to retireve the profile id, convert to a Sightly variable and instantiate another model class -->
+
+
+        <div class="col-md-4 text-center product-list-item">
+          <a href="/content/wknd-jecis/en/shopping/single-product.html?profile=eqsmcz-bl&store=wknd">
+            <div class="simpleCart_shelfItem wrapper">
+              <div class="ribbon-wrapper-red">
+                <div class="ribbon-red">PROMO</div>
+              </div>
+
+
+
+              <img src="/content/dam/we-retail/en/products/activities/hiking/backpack/Frame%201.jpg" class="img-responsive" alt="Cuzco Blue"/>
+
+
+
+              <h3><a href="/content/wknd-jecis/en/shopping/single-product.html?profile=eqsmcz-bl&store=wknd"> Cuzco Blue</a></h3>
+              <div class="price">
+                <h5></h5>
+
+                <button data-prod-discount="0" data-prod-symbol="€" data-prod-price="210.00" data-prod-prof="eqsmcz-bl" class="btn cart item_add view-all-border item_add btn-large">Add To Cart</button>
+                <div class="clearfix"></div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <!-- Testing how to retireve the profile id, convert to a Sightly variable and instantiate another model class -->
+
+        <div class="col-md-4 text-center product-list-item">
+          <a href="/content/wknd-jecis/en/shopping/single-product.html?profile=eqwrsnbd-1&store=wknd">
+            <div class="simpleCart_shelfItem wrapper">
+              <div class="ribbon-wrapper-red">
+                <div class="ribbon-red">PROMO</div>
+              </div>
+
+              <img class="img-responsive" alt="Flying Snowboard"/>
+
+              <h3><a href="/content/wknd-jecis/en/shopping/single-product.html?profile=eqwrsnbd-1&store=wknd"> Flying Snowboard</a></h3>
+              <div class="price">
+                <h5></h5>
+
+                <button data-prod-discount="0" data-prod-symbol="€" data-prod-price="599.00" data-prod-prof="eqwrsnbd-1" class="btn cart item_add view-all-border item_add btn-large">Add To Cart</button>
+                <div class="clearfix"></div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <!-- Testing how to retireve the profile id, convert to a Sightly variable and instantiate another model class -->
+
+        <div class="col-md-4 text-center product-list-item">
+          <a href="/content/wknd-jecis/en/shopping/single-product.html?profile=eqsusuely&store=wknd">
+            <div class="simpleCart_shelfItem wrapper">
+              <div class="ribbon-wrapper-red">
+                <div class="ribbon-red">PROMO</div>
+              </div>
+              <img class="img-responsive" alt="El Greco 8&#39; Shortboard"/>
+
+              <h3><a href="/content/wknd-jecis/en/shopping/single-product.html?profile=eqsusuely&store=wknd"> El Greco 8&#39; Shortboard</a></h3>
+              <div class="price">
+                <h5></h5>
+
+                <button data-prod-discount="0" data-prod-symbol="€" data-prod-price="795.00" data-prod-prof="eqsusuely" class="btn cart item_add view-all-border item_add btn-large">Add To Cart</button>
+                <div class="clearfix"></div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <!-- Testing how to retireve the profile id, convert to a Sightly variable and instantiate another model class -->
+        <div class="col-md-4 text-center product-list-item">
+          <a href="/content/wknd-jecis/en/shopping/single-product.html?profile=eqbisucol-1&store=wknd">
+            <div class="simpleCart_shelfItem wrapper">
+              <div class="ribbon-wrapper-red">
+                <div class="ribbon-red">PROMO</div>
+              </div>
+
+              <img src="/content/dam/we-retail/en/products/activities/biking/source/Chain%20Tool%20In%20Action%201.jpg" class="img-responsive" alt="Compact Chain Tool"/>
+
+              <h3><a href="/content/wknd-jecis/en/shopping/single-product.html?profile=eqbisucol-1&store=wknd"> Compact Chain Tool</a></h3>
+              <div class="price">
+                <h5></h5>
+
+                <button data-prod-discount="0" data-prod-symbol="€" data-prod-price="15.00" data-prod-prof="eqbisucol-1" class="btn cart item_add view-all-border item_add btn-large">Add To Cart</button>
+                <div class="clearfix"></div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <!-- Testing how to retireve the profile id, convert to a Sightly variable and instantiate another model class -->
+
+
+
+
+
+        <div class="col-md-4 text-center product-list-item">
+          <a href="/content/wknd-jecis/en/shopping/single-product.html?profile=eqsmcz-gr&store=wknd">
+            <div class="simpleCart_shelfItem wrapper">
+              <div class="ribbon-wrapper-red">
+                <div class="ribbon-red">PROMO</div>
+              </div>
+
+
+
+              <img src="/content/dam/we-retail/en/products/activities/hiking/backpack/Backpack_H.jpg" class="img-responsive" alt="Cuzco Green"/>
+
+
+
+              <h3><a href="/content/wknd-jecis/en/shopping/single-product.html?profile=eqsmcz-gr&store=wknd"> Cuzco Green</a></h3>
+              <div class="price">
+                <h5></h5>
+
+                <button data-prod-discount="0" data-prod-symbol="€" data-prod-price="210.00" data-prod-prof="eqsmcz-gr" class="btn cart item_add view-all-border item_add btn-large">Add To Cart</button>
+                <div class="clearfix"></div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <!-- Testing how to retireve the profile id, convert to a Sightly variable and instantiate another model class -->
+
+
+
+
+
+        <div class="col-md-4 text-center product-list-item">
+          <a href="/content/wknd-jecis/en/shopping/single-product.html?profile=eqbisuset-blue&store=wknd">
+            <div class="simpleCart_shelfItem wrapper">
+              <div class="ribbon-wrapper-red">
+                <div class="ribbon-red">PROMO</div>
+              </div>
+
+
+
+              <img src="/content/dam/we-retail/en/products/activities/biking/source/Sequoia.jpg" class="img-responsive" alt="Sequoia Blue Helmet TEST"/>
+
+
+
+              <h3><a href="/content/wknd-jecis/en/shopping/single-product.html?profile=eqbisuset-blue&store=wknd"> Sequoia Blue Helmet TEST</a></h3>
+              <div class="price">
+                <h5></h5>
+
+                <button data-prod-discount="0" data-prod-symbol="€" data-prod-price="75.00" data-prod-prof="eqbisuset-blue" class="btn cart item_add view-all-border item_add btn-large">Add To Cart</button>
+                <div class="clearfix"></div>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <!-- Close the content-top1 DIV -->
+        <div class="clearfix"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
